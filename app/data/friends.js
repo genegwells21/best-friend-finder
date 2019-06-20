5f471ee3e26559b71e344505b8971b33612247c3
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-20 17:52:34 -0400

    friends.js added

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+
+
+var friends = [
+    {
+    name: "Jack Canfield",
+    photo:
+        "https://www.bing.com/images/search?view=detailV2&id=679A629BA6F3F6D0647ECB73E89A941EB96BE481&thid=OIP.3EMybHurMbhpArdpsqRT6AHaJ4&mediaurl=http%3A%2F%2Fwww.lifeoptimizer.org%2Fwp-content%2Fuploads%2F2016%2F05%2Fjack-canfield.jpg&exph=900&expw=675&q=jack+canfield&selectedindex=1&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [4, 2, 9, 1, 9, 6, 5, 2, 1, 8]
+    },
+{
+    name: "Mark Victor Hansen",
+    photo: "https://www.bing.com/images/search?view=detailV2&id=F7575D11A5C7B0B70518A42544418DAF911FB29B&thid=OIP.j8_P07NQt5GgBmtgMOU41wHaLH&mediaurl=http%3A%2F%2Fthepmshow.tv%2Fwp-content%2Fuploads%2F2011%2F08%2Fmark_victor_hansen.jpg&exph=768&expw=512&q=mark+victor+hansen&selectedindex=0&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [6, 8, 7, 3, 6, 8, 1, 5, 6, 5]
+
+    },
+    {
+    name: "John Assaraf",
+    photo: "https://www.bing.com/images/search?view=detailV2&id=F898AA9F57EB3A3CDE611E0E692EFE66643DBE00&thid=OIP.vfN93kx9XBixvcsDG9b-CQHaHa&mediaurl=http%3A%2F%2Fwww.myneurogym.com%2Fabout-us%2Fimages%2Fteam%2FJohn-Assaraf.jpg&exph=500&expw=500&q=john+assaraf&selectedindex=5&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [6, 3, 2, 8, 5, 1, 9, 4, 8, 3]
+    },
+    {    
+    name: "Napoleon Hill",
+    photo: "https://www.bing.com/images/search?view=detailV2&id=F898AA9F57EB3A3CDE611E0E692EFE66643DBE00&thid=OIP.vfN93kx9XBixvcsDG9b-CQHaHa&mediaurl=http%3A%2F%2Fwww.myneurogym.com%2Fabout-us%2Fimages%2Fteam%2FJohn-Assaraf.jpg&exph=500&expw=500&q=john+assaraf&selectedindex=5&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [7, 5, 2, 9, 4, 6, 2, 4, 7, 9]
+        },
+    {
+    name: "Earl Nightingale",
+    photo: "https://www.bing.com/images/search?view=detailV2&id=420DC40B17D194701320346D04C71F1C7BEBB254&thid=OIP.LUqZ2IW8B5gJjQcc1J9T4wAAAA&mediaurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F742659420878962688%2FHz5oUraP.jpg&exph=300&expw=300&q=earl+nightingale&selectedindex=1&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [2, 6, 8, 2, 5, 4, 3, 8, 2, 7]
+   },
+   {
+    name: "Brian Tracy",
+    photo: "https://www.bing.com/images/search?view=detailV2&id=87D1F9831C6FFF52386DAFDC981CDEB9B3AEA1F2&thid=OIP.Kk34yjxXkWSNplgm39PUegHaHv&mediaurl=http%3A%2F%2Frightselection.com%2Fevent%2Fwp-content%2Fuploads%2F2016%2F01%2FBrian-Tracy-1.png&exph=650&expw=622&q=brian+tracy&selectedindex=1&ajaxhist=0&vt=0&eim=1,2,6",
+    scores: [5, 8, 3, 5, 2, 7, 4, 7, 9, 1]
+   }
+];
+module.exports = friends;
